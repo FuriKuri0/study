c2d18879abdbd7f68f9d094305c12cbfd2471eb2
FuriKuri <277541105@qq.cpom>
2022-08-03 10:36:10 +0800

    hhe

---
+++ study/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import {useRoutes} from 'react-router-dom'
+// import routes from './routes'
+import 'antd/dist/antd.min.css'
+import './App.css'
+import Nav from './component/nav'
+
+export default function App() {
+    return (
+        <div>
+            <Nav />
+        </div>
+    )
+}
